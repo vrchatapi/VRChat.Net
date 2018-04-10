074fe42e1b4766718fd66ffb43c0a409a97e5541
avail <avail@pomf.se>
2018-04-11 00:46:22 +0200

    update test app

--- VRChatApi/TestClient/Program.cs
+++ VRChatApi/TestClient/Program.cs
@@ -14,11 +14,19 @@ namespace TestClient
         {
             VRChatApi.VRChatApi api = new VRChatApi.VRChatApi("avail", "");
 
-            ConfigResponse config = await api.GetConfig();
+            // remote config
+            ConfigResponse config = await api.RemoteConfig.Get();
 
-            UserResponse user = await api.PerformLogin();
+            // user api
+            UserResponse user = await api.UserApi.Login();
+            //UserResponse userNew = await api.UserApi.Register("someName", "somePassword", "some@email.com");
+            //UserResponse userUpdated = await api.UserApi.UpdateInfo(user.id, null, null, null, new List<string>() { "admin_moderator", "admin_scripting_access", "system_avatar_access", "system_world_access" });
 
-            Console.WriteLine(config.address);
+            // friends
+            //List<UserBriefResponse> friends = await api.FriendsApi.Get(0, 20, true);
+            //NotificationResponse friendRequestResponse = await api.FriendsApi.SendRequest("usr_f8220fc0-e6f9-45ab-8d9f-ae00e8491685", api.UserApi.Username);
+            //string friendDeletionResponse = await api.FriendsApi.DeleteFriend("usr_f8220fc0-e6f9-45ab-8d9f-ae00e8491685");
+            //await api.FriendsApi.AcceptFriend("usr_f8220fc0-e6f9-45ab-8d9f-ae00e8491685");
         }
     }
 }
