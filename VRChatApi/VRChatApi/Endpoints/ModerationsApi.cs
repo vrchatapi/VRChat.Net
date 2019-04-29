eafdfe745f16f16e41e76b14b9e90e2557e16883
Bluscream <admin@timo.de.vc>
2019-04-29 06:50:06 +0200

    update repo

--- VRChatApi/VRChatApi/Endpoints/ModerationsApi.cs
+++ VRChatApi/VRChatApi/Endpoints/ModerationsApi.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using VRChatApi.Classes;
 using VRChatApi.Logging;
 
@@ -17,7 +19,7 @@ namespace VRChatApi.Endpoints
         public async Task<List<PlayerModeratedResponse>> GetPlayerModerations()
         {
             Logger.Trace(() => "Get list of moderations made by current user");
-            HttpResponseMessage response = await Global.HttpClient.GetAsync("auth/user/playermoderations");
+            HttpResponseMessage response = await Global.HttpClient.GetAsync($"auth/user/playermoderations?apiKey={Global.ApiKey}");
 
             List<PlayerModeratedResponse> res = null;
 
@@ -34,7 +36,7 @@ namespace VRChatApi.Endpoints
         public async Task<List<PlayerModeratedResponse>> GetPlayerModerated()
         {
             Logger.Trace(() => "Get list of moderations made against current user");
-            HttpResponseMessage response = await Global.HttpClient.GetAsync("auth/user/playermoderated");
+            HttpResponseMessage response = await Global.HttpClient.GetAsync($"auth/user/playermoderated?apiKey={Global.ApiKey}");
 
             List<PlayerModeratedResponse> res = null;
 
@@ -47,5 +49,38 @@ namespace VRChatApi.Endpoints
 
             return res;
         }
+
+
+        public async Task<NotificationResponse> BlockUser(string userId)
+        {
+            JObject json = new JObject();
+            json["blocked"] = userId;
+            Logger.Debug(() => $"Prepared JSON to post: {json}");
+            StringContent content = new StringContent(json.ToString(), Encoding.UTF8);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = await Global.HttpClient.PostAsync($"auth/user/blocks?apiKey={Global.ApiKey}", content);
+            NotificationResponse res = null;
+            if (response.IsSuccessStatusCode)
+            {
+                var receivedJson = await response.Content.ReadAsStringAsync();
+                Logger.Debug(() => $"JSON received: {receivedJson}");
+                res = JsonConvert.DeserializeObject<NotificationResponse>(receivedJson);
+            }
+            return res;
+        }
+
+        public async Task<string> UnblockUser(string userId, string modId)
+        {
+            HttpResponseMessage response = await Global.HttpClient.DeleteAsync($"user/{userId}/moderations/{modId}?apiKey={Global.ApiKey}");
+            string res = "";
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                Logger.Debug(() => $"JSON received: {json}");
+                res = json;
+            }
+            return res;
+        }
+
     }
 }
